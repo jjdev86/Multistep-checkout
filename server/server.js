3aab9dd2dc7ba025308af64962c4b574526a100b
Jose Valadez <vinmajj@gmail.com>
2019-12-15 23:19:35 -0800

    made small change to server file

--- server/server.js
+++ server/server.js
@@ -18,29 +18,15 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cors());
 
-
-
-// moongodb connection keys
-// const configFolder = require('../config/keys');
-
-// mongoose.connect(configFolder.mongoURI, { useNewUrlParser: true });
-
-// mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
-// mongoose.connection.once('open', () => {
-//   console.log('Connected to the database server.js');
-// });
-
 // Use Routes
 app.use('/', users);
 
 // serve the static files from the build folder to the client
 app.use(express.static(`${__dirname}/../build`));
 
-const port = process.env.PORT || 4000; // process.env.PORT
+const port = process.env.PORT || 3000; // process.env.PORT
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
-
-
 
 
 module.exports = app;
